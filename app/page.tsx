5a6e374ce949f6a13b5c092451d3980c635a0187
Adithya K B <adithyakb93@gmail.com>
2024-11-07 11:46:32 +0530

    feat: Fetch products from firebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,44 @@
+"use client";
 import ProductCard from "@/components/productcard";
-// import { db } from "@/lib/firebase/firebase";
-// import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/lib/firebase/firebase";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 
-export default async function Home() {
-  // try {
-  //   const usersRef = collection(db, "users");
-  //   const docSnap = await getDocs(usersRef);
-  //   docSnap.forEach((doc) => {
-  //     console.log(doc.data());
-  //   });
-  // } catch (e) {
-  //   console.log(e);
-  // }
+interface Product {
+  id: string;
+  productname: string;
+  productprice: number;
+  productimage: string;
+}
+
+export default function Home() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const querySnapshot = await getDocs(collection(db, "products"));
+      const productsArray = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...(doc.data() as Omit<Product, "id">),
+      }));
+      setProducts(productsArray);
+    };
+
+    fetchProducts();
+  }, []);
   return (
     <div className="p-20">
-      <div>
-        <ProductCard />
-      </div>
+      {products.map((product) => (
+        <div
+          key={product.id}
+          className="py-10"
+        >
+          <ProductCard
+            productName={product.productname}
+            productPrice={product.productprice}
+          />
+        </div>
+      ))}
     </div>
   );
 }
--- components/productcard.tsx
+++ components/productcard.tsx
@@ -4,10 +4,18 @@ import React, { useState } from "react";
 import { Button } from "./ui/button";
 import { Minus, Plus } from "lucide-react";
 
-export default function ProductCard() {
+interface ProductCardProps {
+  productName: string;
+  productPrice: number;
+}
+
+export default function ProductCard({
+  productName,
+  productPrice,
+}: ProductCardProps) {
   const [weight, setWeight] = useState(0);
   const [totalCost, setTotalCost] = useState(0);
-  const price = 100;
+  const price = productPrice;
 
   const handleDecrease = () => {
     setWeight((prevWeight) => {
@@ -25,8 +33,12 @@ export default function ProductCard() {
     });
   };
 
+  function capitalizeFirstLetter(name: string) {
+    return String(name).charAt(0).toUpperCase() + String(name).slice(1);
+  }
+
   return (
-    <div className="w-fit border-2 border-green-500 px-3 rounded-md shadow-lg">
+    <div className="w-fit border-2 border-gray-200 px-3 rounded-md shadow-lg">
       <div className="w-full">
         <Image
           src="/banana.png"
@@ -36,34 +48,39 @@ export default function ProductCard() {
           className="w-full"
         />
       </div>
-      <div className="text-center text-lg pb-5">
-        <p>Price: &#8377;{price}(per KG)</p>
+      <div className="text-center text-xl font-medium pb-1">
+        {capitalizeFirstLetter(productName)}
+      </div>
+      <div className="text-center text-lg">
+        <p>&#8377;{price}/Kg</p>
       </div>
       <div className="flex justify-between">
-        <div className="flex flex-col gap-2 text-center">
-          <p>Quantity(in Kgs)</p>
-          <div className="flex gap-2 text-center">
-            <Minus
-              size={24}
-              onClick={handleDecrease}
-            />
-            <p>{weight} Kg</p>
-            <Plus
-              onClick={handleIncrease}
-              size={24}
-            />
+        <div className="flex flex-col py-5 gap-2">
+          <div className="flex flex-col gap-2 text-center">
+            <p>Quantity(in Kgs)</p>
+            <div className="flex gap-2 text-center justify-center">
+              <Minus
+                size={24}
+                onClick={handleDecrease}
+              />
+              <p>{weight} Kg</p>
+              <Plus
+                onClick={handleIncrease}
+                size={24}
+              />
+            </div>
           </div>
+          <Button className="bg-green-500 hover:bg-green-400">Sell</Button>
         </div>
-        <div className="flex flex-col gap-2 text-center">
-          <p>Estimated Price</p>
-          <p>&#8377;{totalCost}</p>
+        <div className="py-5 flex flex-col gap-2">
+          <div className="flex flex-col gap-2 text-center">
+            <p>Estimated Price</p>
+            <p>&#8377;{totalCost}</p>
+          </div>
+          <Button className="bg-green-500 hover:bg-green-400">
+            Add to Sack
+          </Button>
         </div>
-      </div>
-      <div className="py-10 flex justify-between gap-16">
-        <Button className="bg-green-500 hover:bg-green-400 flex-1">Sell</Button>
-        <Button className="bg-green-500 hover:bg-green-400 flex-1">
-          Add to Sack
-        </Button>
       </div>
     </div>
   );
